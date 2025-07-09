7d90c34367f1c0979ede819605a06e93ddf1ed8f
JoNinna <maria.burta@student.upt.ro>
2025-07-09 08:10:03 +0300

    ex7.py

--- HW1/ex7.py
+++ HW1/ex7.py
@@ -10,34 +10,28 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # 1. Scrieți o funcție ce primește ca parametru un url și întoarce timpul de răspuns al paginii (cat dureaza sa primim un răspuns)
-def raspuns_Fisier(url):
+def raspuns(url):
     call_time = time.time()
     call = requests.get(url,verify=False)
-    response_time = time.time() - call_time
-    return f"Timpul de raspuns al site-ului {url} este: {response_time}" 
-
-def raspuns_Lista(url):
-    call_time = time.time()
-    call = requests.get(url,verify=False)
-    response_time = time.time() - call_time
+    response_time = (time.time() - call_time) * 1000
     return response_time
 
 # 2. Adaugati încă o funcție ce primește doi parametrii (un url si un numar de repetari)
-def call_outputFisier(url, iteratii):
-# Funcția va face într-un for un număr de cal-uri către url și va memora timpurile de răspuns într-un fisier text (folosind prima metoda)
-    with open("raspunsuri.txt","a") as file:
-        for iterator in iteratii:
-            context = raspuns(url)
-            file.write(context + "\n")
-
-# Funcția va face într-un for un număr de cal-uri către url și va memora timpurile de răspuns într-o listă (folosind prima metoda)
-# Si va face o medie și va întoarce într-un tuplu 3 valori în următoarea ordine: (min, media, max)
-def call_outputLista(url, iteratii):
+def apel(url, iteratii):
+    # Funcția va face într-un for un număr de cal-uri către url și va memora timpurile de răspuns într-o listă (folosind prima metoda)
     listaTimpRaspuns = []
-    for iterator in iteratii:
-        iterator = raspuns_Lista
+    for iterator in range(int(iteratii)):
+        iterator = raspuns(url)
+        print(f"Timpul de raspuns pentru site-ul {url} este: {iterator} milisecunde")
         listaTimpRaspuns.append(iterator)
+        time.sleep(2)
+
+    # Si va face o medie și va întoarce într-un tuplu 3 valori în următoarea ordine: (min, media, max)
+    media = sum(listaTimpRaspuns)/len(listaTimpRaspuns)
+    minimum = min(listaTimpRaspuns)
+    maximum = max(listaTimpRaspuns)
+    return (minimum, media, maximum)
 
 url = input("Introduceti adresa url: ")
 iteratii = input("Introduceti numarul de iteratii: ")
-call_outputLista(url, iteratii)
+print(f"Minimul, media si maximum-ul de valori de raspuns sunt: {apel(url, iteratii)}")
