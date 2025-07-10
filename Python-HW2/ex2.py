89dda2b8eece941d3d58d1ae3362a541a0099596
JoNinna <maria.burta@student.upt.ro>
2025-07-10 13:06:11 +0300

    small update ex1.py

---
+++ Python-HW2/ex2.py
@@ -0,0 +1,8 @@
+# Faceți un modul de utils in care adaugati 2 metode: 
+# una care face sha256 hash la un string si alta la un fisier. 
+# Testați aceste metode de utils dintr-un alt script de python.
+# Hint:
+# hashlib.sha256(text.encode()).hexdigest()
+# hashlib.sha256(f.read()).hexdigest()
+
+# Folosiți libraria facuta de voi pentru a genera un sha256 hash pentru un fisier de pe disk si comparati-l cu valoarea obtinuta ruland comanda de linux sha256sum.
