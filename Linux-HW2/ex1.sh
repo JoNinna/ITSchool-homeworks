902d53aa5616e59505b1bab5a7e9a70dd65aa543
JoNinna <maria.burta@student.upt.ro>
2025-08-13 15:44:04 +0300

    ex1 complet

--- Linux-HW2/ex1.sh
+++ Linux-HW2/ex1.sh
@@ -3,16 +3,22 @@
 # De adaugat exit codes si validari
 # Faceți un script ce așteaptă (la nesfarsit) după un fisier pe disk sa fie creat (ce fișier doriti). 
 # După ce fișierul a fost create scriptul afișează un mesaj și iese cu succes.
-
-while true; do
-    sleep Infinity
-    if [ -e $1 ]; then
-        echo "Fisierul exista deja pe disk!"
-    else
-        echo >> $1
-        exit 0
-    fi 
-done
-
 # Bonus (dificultate medie): Modificați scriptul să nu aștepte la nesfarsit ci maxim 1 minut. 
 # Dar daca fisierul este create mai devreme de 1 minut scriptul trebuie sa se termine mai devreme.
+
+nume_fisier="ex1.txt"
+timeout=60
+passed=0
+
+while [ $passed -lt $timeout ]; do
+    if [ -f $nume_fisier ]; then
+        echo "Fisierul a fost creat pe disk!"
+        exit 0
+    fi 
+    sleep 1
+    passed=$((passed+1))
+done
+
+echo "Fisierul nu a fost creat in timpul acordat!"
+exit 1
+
