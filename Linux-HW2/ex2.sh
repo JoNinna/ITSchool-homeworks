833496f8b3a77f31aa55b20808369539b74d9c86
JoNinna <maria.burta@student.upt.ro>
2025-08-14 15:54:57 +0300

    ex2 + bonus rezolvat

--- Linux-HW2/ex2.sh
+++ Linux-HW2/ex2.sh
@@ -22,26 +22,22 @@
 # În loc de 2 fișiere comparati o lista de oricât de multe fișiere. 
 # Dacă oricare 2 fișiere din lista sunt diferite intoarce-ti un mesaj de eroare.  
 
-# lista_fisiere=()
-
-args=("$@")
-
 read -p "Cate fisiere doriti sa comparati? " numarf
 
 echo "Introduceti fisierele de comparat: " 
 read -a lista_fisiere
 
-for index in $numarf; do
-    echo "${lista_fisiere[${index}]}"
-    i=$((index+1))
-    echo "${lista_fisiere[${i}]}"
-    # sha1=$(sha256sum "${lista_fisiere[index]}" | awk '{print $1}')
-    # sha2=$(sha256sum "${lista_fisiere[index+1]}" | awk '{print $1}')
+for (( i=0; i<$numarf; i++ )); do
+    for (( j=i+1; j<$numarf; j++ )); do
+        sha1=$(sha256sum "${lista_fisiere[$i]}" | awk '{print $1}')
+        sha2=$(sha256sum "${lista_fisiere[$j]}" | awk '{print $1}')
+        if [[  "$sha1" != "$sha2" ]]; then
+            echo "Continutul este diferit in fisierele: "
+            echo "${lista_fisiere[$j]} si ${lista_fisiere[$i]}" 
+            exit 1
+        fi
+    done
 done
 
-# for $item in $numarf; do  
-#     read -p "Introduceti urmatorul fisier de comparat: " urmatorulf
-#     urmatorulf=$args[item]
-#     echo "$urmatorulf"
-#     item=$(item+1)
-
+echo "Continutul este acelasi in toate fisierele!"
+exit 0
