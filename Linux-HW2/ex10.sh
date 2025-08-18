eadada99d6e7d4938081c347047292c064947252
JoNinna <maria.burta@student.upt.ro>
2025-08-18 18:24:35 +0300

    ex10 complet

--- Linux-HW2/ex10.sh
+++ Linux-HW2/ex10.sh
@@ -4,8 +4,12 @@
 # Faceți acest lucru în mod automat de fiecare data cand se porneste o sesiune cu login pentru userul curent. 
 # Adaugati un script în acel director și încercați să-l executați de oriunde.
 
-cale="$HOME/bin-itschools/"
-
-if [ -d "$cale" ]; then
-    export PATH="$PATH:$cale"
+# Scriptul este adaugat direct in ~/.bashrc, si verifica ca bin-itschools exista
+to_run='
+if [ -d "$HOME/bin-itschools" ]; then
+    export PATH="$PATH:$HOME/bin-itschools";
 fi
+'
+
+echo $to_run >> "$HOME/.bashrc"
+echo "[OK] calea ~/bin-itschools/ a fost adaugata cu succes in PATH"
