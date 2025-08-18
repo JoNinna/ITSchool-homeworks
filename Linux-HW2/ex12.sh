ac5ec4cc40cadfad692a2c8aa5accfe7f1be3148
JoNinna <maria.burta@student.upt.ro>
2025-08-18 21:33:45 +0300

    ex12 complet

--- Linux-HW2/ex12.sh
+++ Linux-HW2/ex12.sh
@@ -9,8 +9,9 @@ for file in $check_folder/*; do
     name=$(basename "$file")
     check_extension="${name##*.}"
     if [ $check_extension == "sh" ]; then
-        if [ -z $(grep "#! /bin/bash" "$file") ]; then
-            echo "Fisierul $file nu are linia de shebang!"
+        if [[ -z $(grep "#! /bin/bash" "$file") ]]; then
+            sed -i '1i#! /bin/bash' $file
+            echo "In fisierul $file a fost adaugata linia de shebang!"
         else
             echo "Fisierul $file are linia de shebang!"
         fi
